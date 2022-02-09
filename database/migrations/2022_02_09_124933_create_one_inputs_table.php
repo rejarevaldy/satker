089d86142ca4c01e97db082f3b01e692044ef195
revaldy-30 <rejarevaldy30@gmail.com>
2022-02-09 21:03:25 +0800

    migration

---
+++ database/migrations/2022_02_09_124933_create_one_inputs_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOneInputsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('one_inputs', function (Blueprint $table) {
+            $table->id();
+            $table->string('digit');
+            $table->string('kd_kro');
+            $table->string('kd_ro');
+            $table->enum('bidang' , ['Umum', 'PPA I', 'PPA II', 'SKKI', 'PAPK', 'Admin']);
+            $table->string('nama_ro');
+            $table->string('capaian_ro')->nullable()->default(null);
+            $table->integer('volume_target');
+            $table->enum('satuan', ['Kegiatan', 'Dokumen' ,'Pegawai', 'Rekomendasi' ,'ISO', 'Satker' , 'Laporan' ,'KPPN' , '-' , 'Bulan Layanan']);
+            $table->integer('volume_jumlah')->nullable()->default(0);
+            $table->float('rvo');
+            $table->float('rvo_maksimal');
+            $table->bigInteger('pagu');
+            $table->bigInteger('rp');
+            $table->float('capaian');
+            $table->bigInteger('sisa');
+            $table->softDeletesTz($column = 'deleted_at', $precision = 0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('one_inputs');
+    }
+}
---
+++ database/migrations/2022_02_09_125134_create_two_inputs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTwoInputsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('two_inputs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('one_input_id')->onDelete('cascade')->nullable();
+            $table->integer('volume_capaian')->nullable();
+            $table->string('uraian')->nullable()->default(null);
+            $table->string('nomor_dokumen')->nullable()->default(null);
+            $table->timestampTz('tanggal')->nullable()->default(null);
+            $table->string('file')->nullable()->default(null);
+            $table->softDeletesTz($column = 'deleted_at', $precision = 0);
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('two_inputs');
+    }
+}
