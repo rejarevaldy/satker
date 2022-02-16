9526349f8011dee17dca91609d9feb6ce3c88790
Jireii <mazhn34@gmail.com>
2022-02-16 12:31:34 +0800

    fix ifelse

--- resources/views/input/show.blade.php
+++ resources/views/input/show.blade.php
@@ -161,20 +161,22 @@ foreach ($datas2 as $data2) {
         $loop++;
 
 
-    if (auth()->user()->role == 'Monitor') {
+    if (auth()->user()->role == 'Monitoring') {
         $config = [
         'data' => $query,
         'order' => [[0, 'asc']],
         'columns' => [['className' => 'text-center'],['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center']],
         'language' => ['url' => 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/id.json'],
     ];
-    } else {
+    } elseif (auth()->user()->role == 'Satker') {
         $config = [
         'data' => $query,
         'order' => [[0, 'asc']],
         'columns' => [['className' => 'text-center'],['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center'], ['className' => 'text-center']],
         'language' => ['url' => 'https://cdn.datatables.net/plug-ins/1.11.3/i18n/id.json'],
     ];
+    } else {
+        $config = [];
     }
 }
 
