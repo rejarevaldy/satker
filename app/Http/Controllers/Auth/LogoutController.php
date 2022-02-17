449676c31a198c23847de17c933af0cdc39426d4
Reja Revaldy F <rejarevaldy30@gmail.com>
2022-02-17 22:48:37 +0800

    Update LogoutController.php

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -9,11 +9,9 @@ use Illuminate\Support\Facades\Auth;
 class LogoutController extends Controller
 {
     public function logout(Request $request){
-      Auth::logout();
-
-      $request->session()->invalidate();
-      $request->session()->regenerateToken();
-
-      return redirect('/login');
+        $request->session()->forget('year');
+        auth()->logout();
+        
+        return redirect('/login/');
     }
 }
